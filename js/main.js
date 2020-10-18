97dac76258100c92c05a326b3afe5641b98f11d5
thanlonsmith <thanlon@163.com>
2020-10-18 18:38:23 +0800

    First commit.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 $('table').removeClass('table-striped table-hover')
 $('table').addClass('table-bordered')
+$(function () {
+    $('button.close').click(function () {
+        $("#support_us").slideUp("slow");
+    })
+
+    function support_us() {
+        $("#support_us").slideDown("slow");
+    }
+
+    setInterval(support_us, 30000)
+})
