7a799916449ddaefa6e0266ec5d2401bbded2b82
erics1996 <erics1996@yeah.net>
2020-10-30 20:02:37 +0800

    Released v1.0.0 firstly.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
-$('table').removeClass('table-striped table-hover')
-$('table').addClass('table-bordered')
 $(function () {
-    $('button.close').click(function () {
-        $("#support_us").slideUp("slow");
-    })
+  $('table').removeClass('table-striped table-hover')
+  $('table').addClass('table-bordered')
+  $('button.close').click(function () {
+    $("#ads").slideUp("slow");
+  })
 
-    function support_us() {
-        $("#support_us").slideDown("slow");
-    }
+  function ads() {
+    $("#ads").slideDown("slow");
+  }
 
-    setInterval(support_us, 30000)
+  setInterval(ads, 300000)
+  // setInterval(ads, 2000)
+  // 滚动公告start
+  var num = $(".ul").find("li").length;
+  if (num > 1) {
+    setInterval(function () {
+      $('.ul').animate({
+        marginTop: "-30px"
+      },function () {
+        $(this).css({
+          marginTop: "0"
+        }).find("li:first").appendTo(this);
+      });
+    }, 4000);
+  }
+  // 滚动公告stop
 })
