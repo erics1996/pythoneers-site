9ce48059055d68446b1a58c181efb10767bc39f4
erics1996 <erics1996@yeah.net>
2020-11-18 03:20:38 +0800

    Updated 11.18,2020

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function () {
+    $('table').removeClass('table-striped table-hover')
+    $('table').addClass('table-bordered')
+    $('button.close').click(function () {
+        $("#ads").slideUp("slow");
+    })
+
+    function ads() {
+        $("#ads").slideDown("slow");
+    }
+
+    setInterval(ads, 300000)
+    // setInterval(ads, 2000)
+    // 滚动公告start
+    var num = $(".ul").find("li").length;
+    if (num > 1) {
+        setInterval(function () {
+            $('.ul').animate({
+                marginTop: "-30px"
+            }, 500, function () {
+                $(this).css({marginTop: "0"}).find("li:first").appendTo(this);
+            });
+        }, 4000);
+    }
+    // 滚动公告stop
+})
