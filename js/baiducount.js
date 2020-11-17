140e45f14169ad4fe99794b4d8b2676e7dccfd03
erics1996 <erics1996@yeah.net>
2020-11-18 02:57:49 +0800

    Delete old project.

--- js/baiducount.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?0678dc24e9c045f430e824c7f12d1f28";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
